e21b4e5cef132081c8d6eff5660948072c0c6dc2
Abhishek choudhary <135946713+Abhishekjat004@users.noreply.github.com>
2024-07-30 20:52:21 +0530

    Add files via upload

---
+++ jarvis_project/music_library.py
@@ -0,0 +1,4 @@
+music = {"mahadev":"https://www.youtube.com/watch?v=19hh35ICYPc",
+         "sad":"https://www.youtube.com/watch?v=jHKnLzdc5Rg",
+         "jug":"https://www.youtube.com/watch?v=Irw8llgSC3s",
+         "asha":"https://www.youtube.com/watch?v=T747MChwshU"}
